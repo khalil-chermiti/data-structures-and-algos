a6b3ae7163da0c670f9f1ff66ae202ee8e2f1b13
khalil chermiti <khalil666chermiti@gmail.com>
2022-07-01 22:37:16 +0100

    implementing a queue data structure using a singly linked list

---
+++ stacksAndQueues/queues.js
@@ -0,0 +1,45 @@
+/*
+implementing queue data structure on top of a singly linked list
+insertion and removal are O(1)
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  // O(1)
+  enqueue(val) {
+    var newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  // O(1)
+  dequeue() {
+    if (!this.first) return null;
+
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
