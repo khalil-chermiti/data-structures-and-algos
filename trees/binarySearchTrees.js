ccef98cc8606d2993fb46157927373be815ec61a
khalil chermiti <khalil666chermiti@gmail.com>
2022-07-02 02:02:14 +0100

    implemented different algorithms for tree traversal in js : * breadth first search (it/rec) * depth first search : - preorder - inorder - postorder

--- trees/binarySearchTrees.js
+++ trees/binarySearchTrees.js
@@ -1,5 +1,3 @@
-// binary search tree
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -61,22 +59,154 @@ class BinarySearchTree {
 
     return false;
   }
+
+  // level order breadth first search
+  BFS() {
+    if (this.root === null) return [];
+    const nodesQueue = [];
+    const nodes = [];
+    let pointer = null;
+
+    nodesQueue.push(this.root);
+
+    while (nodesQueue.length) {
+      pointer = nodesQueue.shift();
+      nodes.push(pointer.value);
+
+      if (pointer.left) nodesQueue.push(pointer.left);
+      if (pointer.right) nodesQueue.push(pointer.right);
+    }
+
+    return nodes;
+  }
+
+  BFS_REC() {
+    if (this.head === null) return [];
+
+    const nodes = [];
+    const nodesQueue = [];
+    nodesQueue.push(this.root);
+
+    (function recurse() {
+      if (!nodesQueue.length) return;
+
+      let node = nodesQueue.shift();
+      nodes.push(node.value);
+
+      // we add all nodes with the same level to the queue in order
+      if (node.left) nodesQueue.push(node.left);
+      if (node.right) nodesQueue.push(node.right);
+
+      recurse();
+    })();
+
+    return nodes;
+  }
+
+  DFS_PREORDER() {
+    let nodes = [];
+
+    (function recurse(node) {
+      if (!node) return;
+
+      // push current nodes value to the array
+      nodes.push(node.value);
+
+      // deep left recursion
+      recurse(node.left);
+
+      // deep right recursion
+      recurse(node.right);
+    })(this.root);
+
+    return nodes;
+  }
+
+  DFS_POSTORDER() {
+    let nodes = [];
+
+    (function recurse(node) {
+      if (!node) return;
+
+      // deep left recursion
+      recurse(node.left);
+
+      // deep right recursion
+      recurse(node.right);
+
+      // lastly push current nodes value to the array
+      nodes.push(node.value);
+    })(this.root);
+
+    return nodes;
+  }
+
+  DFS_INORDER() {
+    let nodes = [];
+
+    (function recurse(node) {
+      if (!node) return;
+
+      // deep left recursion
+      recurse(node.left);
+
+      // lastly push current nodes value to the array
+      nodes.push(node.value);
+
+      // deep right recursion
+      recurse(node.right);
+    })(this.root);
+
+    return nodes;
+  }
 }
 
 const BST = new BinarySearchTree();
-BST.insert(1);
+BST.insert(4);
 BST.insert(10);
-BST.insert(3);
+BST.insert(1);
 BST.insert(40);
 BST.insert(2);
+BST.insert(7);
+BST.insert(0);
 
-BST.find(0);
+//               4
+//         +------------+
+//         1            10
+//      +------+     +------+
+//      0      2     7      40
 
-// console.log(BST.root);
+console.log("search node.val === 1 :\n", BST.find(1));
+
+console.log("BFS  :", JSON.stringify(BST.BFS()));
+console.log("BFS recursive :", JSON.stringify(BST.BFS_REC()));
+
+console.log("DFS preorder :", JSON.stringify(BST.DFS_PREORDER()));
+console.log("DFS postorder :", JSON.stringify(BST.DFS_POSTORDER()));
+console.log("DFS inorder :", JSON.stringify(BST.DFS_INORDER()));
 
 /*
-insertion and searching is O(log(n))  
-but in worst cases we would have a one sided BST
-which is more like a linear data structure so we may 
-end up with O(n)
+! time complexity :
+* insertion : O(log(n))
+* searching : O(log(n))  
+* traversal : O(n) => we have to visit each node
+
+=================================================
+
+! space complexity : (BFS vs DFS)
+
+* it depends on the shape of the tree : 
+
+wide trees : a tree with a lot of children 
+* BFS : the more children the bigger is the queue so time complexity increases
+* DFS : number of children in a node doesn't matter because we traverse vertically 
+* so the call stach won't take as much space as for a queue
+
+narrow trees : like in case of a onsided tree , when it grows in height not in width 
+* BFS : the number of children will be small , so the queue won't take much memory
+* DFS : because of the long depth of the tree , we will have a long call stack wich will 
+* require much more memory than a queue
+
+DFS inorder => in case of a binary tree it returns a sorted array of nodes 
+DFS preorder => can be used to copy a tree (rebuild it from the outputted array of nodes)
 */
